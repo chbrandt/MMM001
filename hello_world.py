8885fa745de00f9cd4c335236c2834e3ad8e634f
Carlos H Brandt <carloshenriquebrandt@gmail.com>
2019-09-18 09:12:45 +0200

    Use try-n-catch in hello world

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,13 @@
+# Because no programming course really exist without a "Hello World!"
 import sys
 
 def greet_user(name):
     return "Hello {}!".format(name.upper())
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: {} <name>".format(sys.argv[0]))
-    else:
+    try:
         res = greet_user(sys.argv[1])
         print(res)
+    except:
+        print("Usage: {} <name>".format(sys.argv[0]))
+        
